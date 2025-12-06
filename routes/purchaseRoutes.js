946a6341329e8e138a23cbd196e2ed29e307d871
novahogar2025-gif <novahogar2025@gmail.com>
2025-12-06 02:49:08 -0600

    Update purchaseRoutes.js

--- routes/purchaseRoutes.js
+++ routes/purchaseRoutes.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const purchaseController = require('../controllers/compra.controller');
 const { verifyToken } = require('../middleware/jwt.middleware');
-const PurchaseController = require('../controllers/compra.controller');
 
-// POST /api/purchase/complete - COMPLETA TODO EN UN SOLO PASO
-router.post('/complete', verifyToken, purchaseController.completePurchase);
+// Procesar compra
+router.post('/process', verifyToken, purchaseController.processPurchase);
+
+// Finalizar compra (enviar PDF)
+router.post('/finalize', verifyToken, purchaseController.finalizePurchase);
+
+// Obtener órdenes del usuario
+router.get('/orders', verifyToken, purchaseController.getUserOrders);
+
+// Obtener detalles de una orden específica
+router.get('/orders/:id', verifyToken, purchaseController.getOrderDetails);
+
+// Reenviar factura por email
+router.post('/send-invoice/:id', verifyToken, purchaseController.resendInvoice);
 
 module.exports = router;
-
